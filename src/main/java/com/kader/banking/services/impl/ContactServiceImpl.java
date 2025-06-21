fc789210a6ca19ef2558e909f84aff987b54bd2f
Kader <abdoulkaderidde@gmail.com>
2025-06-21 14:12:17 +0200

    Contact Service Implementation

---
+++ src/main/java/com/kader/banking/services/impl/ContactServiceImpl.java
@@ -0,0 +1,38 @@
+package com.kader.banking.services.impl;
+
+
+@Service
+@RequiredArgsConstructor
+public class ContactServiceImpl implements ContactService{
+
+    private final ContactRepository repository;
+    private final ObjectsValidator<ContactDto> validator;
+
+    @Override
+    public int save(ContactDto dto){
+        validator.validate(dto);
+        Contact contact = ContactDto.toEntity(dto);
+        return repository.save(contact).getId();
+    }
+
+    @Override
+    public List<ContactDto> findAll(){
+        return repository.findAll()
+                .stream()
+                .map(ContactDto::fromEntity)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public ContactDto findById(int id){
+        return repository.findById(id)
+                .map(ContactDto::fromEntity)
+                .orElseThrow(() -> new EntityNotFoundException("No contact was found with the provided ID : " + id));
+    }
+
+    @Override
+    public void delete(int id){
+
+        return repository.deleteById(id);
+    }
+}
