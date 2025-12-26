9390a122c6372927837a0f377b70fd4226faff4a
Kader <abdoulkaderidde@gmail.com>
2025-12-26 19:24:20 +0100

    Implémentation du Statistique Controller

---
+++ src/main/java/com/kader/banking/controllers/StatisticsController.java
@@ -0,0 +1,47 @@
+package com.kader.banking.controllers;
+
+
+import com.kader.banking.services.StatisticsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping g("/adresses")
+@RequiredArgsConstructor structor
+public class StatisticsController {
+
+    private final StatisticsService service;
+
+    @GetMapping("/sum-by-date/{user-id}")
+    public ResponseEntity<Map<LocalDate, BigDecimal>> findSumTransactionsByDate(
+            @PathVariable("user-id") Integer userId,
+            @RequestParam("start-date") LocalDate startDate,
+            @RequestParam("end-date") LocalDate endDate
+    ){return ResponseEntity.ok(service.findSumTransactionsByDate(startDate, endDate, userId));
+    }
+
+
+    @GetMapping("/account-balance/{user-id}")
+    public ResponseEntity<BigDecimal> getAccountBalance(
+            @PathVariable("user-id") Integer userId){
+        return ResponseEntity.ok(service.getAccountBalance(userId));
+    }
+
+
+    @GetMapping("/highest-transfer/{user-id}")
+    public ResponseEntity<BigDecimal> highestTransfer(
+            @PathVariable("user-id") Integer userId){
+
+        return ResponseEntity.ok(service.highestTransfer(userId));
+    }
+
+
+    @GetMapping("/highest-deposit/{user-id}")
+    public ResponseEntity<BigDecimal> highestDeposit(
+            @PathVariable("user-id") Integer userId){
+
+        return ResponseEntity.ok(service.highestDeposit(userId));
+
+    }
+}
---
+++ src/main/java/com/kader/banking/controllers/TransactionController.java
@@ -0,0 +1,54 @@
+package com.kader.banking.controllers;
+import com.kader.banking.dto.TransationDto
+
+import com.kader.banking.services.TransactionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transaction")
+@RequiredArgsConstructor
+public class TransactionController {
+
+    private final TransactionService service;
+
+
+    @PostMapping("/")
+    public ResponseEntity<Integer> save(
+            @RequestBody TransactionDto transactionDto
+    ){
+        return ResponseEntity.ok(service.save(transactionDto));
+    }
+
+
+    @GetMapping("/")
+    public ResponseEntity<List<TransactionDto>> findAll(){
+        return ResponseEntity.ok(service.findAll());
+    }
+
+    @GetMapping("/{address-id}")
+    public ResponseEntity<TransactionDto> findById(
+            @PathVariable("address-id") Integer transactionId
+    ){
+        return ResponseEntity.ok(service.findById(transactionId));
+    }
+
+    @DeleteMapping("/{transaction-id}")
+    public ResponseEntity<Void> delete(
+            @PathVariable("transaction-id") Integer transactionId
+    ){
+        service.delete(transactionId);
+        return ResponseEntity.accepted().build();
+    }
+
+    @GetMapping("/users/{user-id}")
+    public ResponseEntity<List<TransactionDto>> findAllByUserId(
+            @PathVariable("user-id") Integer userId
+    ){
+        return ResponseEntity.ok(service.findAllByUserId());
+    }
+
+
+}
